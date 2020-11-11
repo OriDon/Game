484ce1ba5388cb50f0e3f59a032f777b340b8e6a
Ori Don <ori.don@protonmail.com>
2020-11-11 14:03:32 +1000

    created basic sprites and main and conf files

---
+++ untitled/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+  t.window.title = "Untitled Game"
+  t.window.width = 1280
+  t.window.height = 720
+  t.modules.touch = false
+  t.modules.joystick = false
+  t.window.icon = "assets/icon.png"
+end
--- untitled/main.lua
+++ untitled/main.lua
@@ -1,3 +1,16 @@
+local playerSprite
+
+function love.load()
+  love.graphics.setBackgroundColor(142/255,142/255,142/255,100/100)
+  playerSprite = love.graphics.newImage("assets/player.png")
+end
+function love.update()
+  
+end
 function love.draw()
-  love.graphics.print("Hello World",0,0)
+  love.graphics.draw(playerSprite)
+  
+end
+function love.quit()
+  
 end
