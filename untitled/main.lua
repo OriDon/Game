9669ef39d8534a5db2766d97340d7d4754f60296
Ori Don <ori.don@protonmail.com>
2020-11-12 09:12:06 +1000

    chanhged

--- untitled/main.lua
+++ untitled/main.lua
@@ -1,14 +1,12 @@
-local playerSprite
+
 
 function love.load()
-  love.graphics.setBackgroundColor(142/255,142/255,142/255,100/100)
-  playerSprite = love.graphics.newImage("assets/player.png")
+
 end
 function love.update()
   
 end
 function love.draw()
-  love.graphics.draw(playerSprite)
   
 end
 function love.quit()
